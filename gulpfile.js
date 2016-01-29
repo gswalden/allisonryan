7251524a30d41ee14470aaa00f69fae7da90db2f
Greg Walden <greg.walden@makerbot.com>
2016-01-28 20:59:13 -0500

    update seo

--- gulpfile.js
+++ gulpfile.js
@@ -12,54 +12,54 @@ var sourcemaps  = require('gulp-sourcemaps');
 
 // browser-sync task for starting the server.
 gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: './'
-        }
-    });
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  });
 });
 
 gulp.task('less', function () {
-    return gulp.src('css/src/*.less')
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(autoprefixer())
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('css/dist'))
-        .pipe(reload({stream:true}))
-        .pipe(notify('Finished file: <%= file.relative %>'));
+  return gulp.src('css/src/*.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('css/dist'))
+    .pipe(reload({stream:true}))
+    .pipe(notify('Finished file: <%= file.relative %>'));
 });
 
 gulp.task('js', function () {
-    return gulp.src('js/src/*.js')
-        .pipe(plumber())
-        // .pipe(jshint())
-        // .pipe(jshint.reporter('default'))
-        .pipe(sourcemaps.init())
-        // .pipe(babel())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('maps'))
-        .pipe(gulp.dest('js/dist'))
-        .pipe(reload({stream:true}))
-        .pipe(notify('Finished file: <%= file.relative %>'));
+  return gulp.src('js/src/*.js')
+    .pipe(plumber())
+    // .pipe(jshint())
+    // .pipe(jshint.reporter('default'))
+    .pipe(sourcemaps.init())
+    // .pipe(babel())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest('js/dist'))
+    .pipe(reload({stream:true}))
+    .pipe(notify('Finished file: <%= file.relative %>'));
 });
 
 gulp.task('jade', function () {
-    return gulp.src('jade/index.jade')
-        .pipe(plumber())
-        .pipe(jade())
-        .pipe(gulp.dest(''))
-        .pipe(reload({stream:true}))
-        .pipe(notify('Finished file: <%= file.relative %>'));
+  return gulp.src('jade/index.jade')
+    .pipe(plumber())
+    .pipe(jade())
+    .pipe(gulp.dest(''))
+    .pipe(reload({stream:true}))
+    .pipe(notify('Finished file: <%= file.relative %>'));
 });
 
 gulp.task('bs-reload', function () {
-    browserSync.reload();
+  browserSync.reload();
 });
 
 // Default task to be run with `gulp`
 gulp.task('default', ['jade', 'less', 'browser-sync'], function () {
-    gulp.watch('css/src/*.less', ['less']);
-    gulp.watch('jade/*.jade', ['jade']);
-    gulp.watch('js/src/*.js', ['js']);
+  gulp.watch('css/src/*.less', ['less']);
+  gulp.watch('jade/*.jade', ['jade']);
+  gulp.watch('js/src/*.js', ['js']);
 });
